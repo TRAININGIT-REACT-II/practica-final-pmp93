0f79cc73bf281de764d64bc73050b07248df1d31
pmp93 <pedro.martinez@ts-iberia.com>
2021-11-16 15:45:29 +0100

    creado el Login a falta de hacerlo funcional

--- client/src/components/Status.js
+++ client/src/components/Status.js
@@ -1,11 +1,35 @@
 import "./Status.css";
+import { useEffect, useState, useRef } from "react";
+import { render } from "react-dom";
+import { EditNotes, ListNotes } from "./Notes";
 
 // Mostramos si el servidor funciona o no.
-const Status = ({ status }) => {
-  const css = status ? "status status-ok" : "status";
-  const text = status ? "OK" : "NO CONECTA";
+const Status = () => {
+  const [estado, setStatus] = useState(false);
+  const [loading, setLoading] = useState(true);
+  const css = estado ? "status status-ok" : "status";
+  const text = estado ? "OK" : "NO CONECTA";
 
-  return <span className={css}>{text}</span>;
+  useEffect(() => {
+    fetch("/api")
+      .then((res) => res.json())
+      .then((data) => {
+        setStatus(data.status === "ok");
+      })
+      .finally(() => setLoading(false));
+  }, []);
+
+  // Cargamos el estado del servidor
+  return (
+    <>
+      <h1>Curso de React de TrainingIT</h1>
+      <p>
+        Estado del servidor:
+        {loading ? " Cargando..." : <span className={css}>{text}</span>}
+      </p>
+     
+    </>
+  );
 };
 
 export default Status;
